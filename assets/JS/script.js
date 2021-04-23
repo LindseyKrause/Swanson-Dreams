0e58c7883e6c9cdc845fd642ce3db0a8313aa397
Robert Schwartz <bschwartz23@gmail.com>
2021-04-22 20:21:35 -0700

    psuedo JS

---
+++ assets/JS/script.js
@@ -0,0 +1,31 @@
+//Global Variables
+
+
+
+//Capture User Input - submit button 
+
+
+
+//Save User Input in local storage
+
+
+//send User Input to text analyzer 
+
+
+//return analyzed text key word
+
+
+//save Analyzed text key word to local storage
+
+
+//send keyword to Ron quotes API
+
+
+//return Ron quote
+
+
+//save Ron Quote to local storage
+
+
+
+//append Ron quote to new quote area
