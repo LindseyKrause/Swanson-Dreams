95a0320e7890e291d79fcd1635d473326330ac07
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-04-25 17:05:03 -0700

    functions working

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,46 +2,48 @@
 var searchButton = document.querySelector(".button");
 var userInput = document.querySelector(".textarea");
 var ronQuote = document.querySelector("#ronQuote");
-
+var keywordReturn = "";
 
 //Capture User Input - submit button 
 searchButton.addEventListener("click", function () {
+    if (userInput.value.length <= 25) {
+alert("Please enter more than 25 characters");
+    }
+    else {
     console.log("search button clicked");
     console.log(userInput.value);
     var textTerm = userInput.value;
-    sendToText(textTerm);
+    // sendToText(textTerm);
     sendToRon();
     sendToText2(textTerm);
-})
+}})
 
-
-//Save User Input in local storage
-
-
-//send User Input to text analyzer 
-sendToText = function (searchTerm) {
-    //return analyzed text key word
-    fetch("https://aylien-text.p.rapidapi.com/concepts?text=" + searchTerm + "&language=en", {
+sendToText2 = function (searchTerm) {
+    fetch("https://aylien-text.p.rapidapi.com/entities?text=" + searchTerm + "&language=en", {
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "d8b02f9ee6mshafd2f4ddfa99997p12a03cjsn241b3ee6e0ac",
             "x-rapidapi-host": "aylien-text.p.rapidapi.com"
         }
     })
-        .then(response => {
-            return response.json();
-        }).then(data => {
-            console.log("textapi");
-            console.log(data);
-
-
+    .then(response => {
+        return response.json();
+    })
+        .then(data => {
+            console.log("text api 2");
+            keywordReturn = data.entities.keyword[0];
+            console.log(keywordReturn + "!");
         })
+        // var keyword = data.entities;
+        // console.log(keyword);
         .catch(err => {
             console.error(err);
         });
+        // let entity = data.entities.keyword[0];
+        // console.log(entity);
 }
 sendToRon = function (searchRon) {
-    fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+    fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes/search?q=" + keywordReturn)
         .then(response => {
             return response.json();
         }).then(data => {
@@ -53,28 +55,8 @@ sendToRon = function (searchRon) {
             console.error(err);
         });
 }
-// sendToText2 = function (searchTerm) {
-//     fetch("https://aylien-text.p.rapidapi.com/entities?text=" + searchTerm + "&language=en", {
-//         "method": "GET",
-//         "headers": {
-//             "x-rapidapi-key": "d8b02f9ee6mshafd2f4ddfa99997p12a03cjsn241b3ee6e0ac",
-//             "x-rapidapi-host": "aylien-text.p.rapidapi.com"
-//         }
-//     })
-//     .then(response => {
-//         return response.json();
-//     })
-//         .then(data => {
-//             console.log("text api 2");
-//             console.log(data);
-//         })
-//         // var keyword = data.entities;
-//         // console.log(keyword);
-//         .catch(err => {
-//             console.error(err);
-//         });
-        
-// }
+
+//Save User Input in local storage
 
 
 //save Analyzed text key word to local storage
